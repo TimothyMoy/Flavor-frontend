065f32eb19051e5553ae69224835abf241570dc3
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-15 16:45:24 -0700

    installed reactstrap and added home page

---
+++ flavor-app/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home () {
+  return(
+    <div>
+      <h1>
+        Create and Save your favorite recipes.
+      </h1>
+      <p>
+        Find and share everyday cooking inspiration on Flavor. Discover and add recipes based on the food you love.
+      </p>
+    </div>
+  )
+}
+
+export default Home;
