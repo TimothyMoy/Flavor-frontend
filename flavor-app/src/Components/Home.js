3f320b57c8822bea1d1ebc51d12ba10635b31dd2
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-15 16:59:38 -0700

    added css and included react strap components

--- flavor-app/src/Components/Home.js
+++ flavor-app/src/Components/Home.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Container, Col, Button, Row } from 'reactstrap';
 
 function Home () {
   return(
-    <div>
-      <h1>
-        Create and Save your favorite recipes.
-      </h1>
-      <p>
-        Find and share everyday cooking inspiration on Flavor. Discover and add recipes based on the food you love.
-      </p>
-    </div>
+    <Container className="hero">
+      <Row>
+        <Col xs="6">
+          <h1>
+            Create and Save your favorite recipes.
+          </h1>
+          <p>
+            Find and share everyday cooking inspiration on Flavor. Discover and add recipes based on the food you love.
+          </p>
+          <Button className="btn-primary">
+            <a href="/recipes">
+              View Recipes
+            </a>
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
