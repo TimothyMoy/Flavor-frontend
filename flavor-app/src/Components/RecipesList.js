dd727aa11c21739d02962cdfca661be10b66374a
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-22 17:17:16 -0700

    generated sample recipe card

--- flavor-app/src/Components/RecipesList.js
+++ flavor-app/src/Components/RecipesList.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, ListGroup, Col, Button, Row, Card, CardTitle, CardImg, CardBody, CardText } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class RecipesList extends Component {
   render(){
     return(
-      <div>RecipesList</div>
+      <Container>
+        <div>
+          <h3>New Recipes</h3>
+          <ListGroup>
+            <TransitionGroup className="recipes-list">
+              <Row>
+              {/* {recipes.map(({_id,name, picture, subtitle}) => ( */}
+                <CSSTransition timeout={500} classNames="fade">
+                      <Col xs="3">
+                      <Card>
+                      <CardTitle>name</CardTitle>
+                      <CardImg top width="10%" src="{`${picture}`} "alt="Food image cap" />
+                      <CardBody>
+                      <CardText> subtitle</CardText>
+                      <Button className="btn-primary"><Link to="/recipe">View Recipe</Link></Button>
+                      </CardBody>
+                      </Card>
+                      </Col>
+                </CSSTransition>
+              {/* ))} */}
+              </Row>
+            </TransitionGroup>
+          </ListGroup>
+        </div>
+      </Container>
     )
   }
 }
