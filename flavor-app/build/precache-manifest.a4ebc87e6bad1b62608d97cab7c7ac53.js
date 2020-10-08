f9424ac523505d6b2a91ff71b0a987610d8d31df
TimothyMoy <Timothy.z.moy@gmail.com>
2020-10-08 15:38:58 -0700

    update git ignore to test push

---
+++ flavor-app/build/precache-manifest.a4ebc87e6bad1b62608d97cab7c7ac53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f384a5eb361bc9745cdf65bb633411e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c8085f90ca7c9b76ea",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3f336c4da57630f83f35",
+    "url": "/static/css/main.6eeab678.chunk.css"
+  },
+  {
+    "revision": "38c8085f90ca7c9b76ea",
+    "url": "/static/js/2.3e077931.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3e077931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f336c4da57630f83f35",
+    "url": "/static/js/main.6203c5f1.chunk.js"
+  },
+  {
+    "revision": "87056e3cf37d102b2305",
+    "url": "/static/js/runtime-main.faf8497e.js"
+  }
+]);
