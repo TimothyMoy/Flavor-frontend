1a2ccce5db0f6f22dd76c72361245a2c5a7f4fde
TimothyMoy <Timothy.z.moy@gmail.com>
2020-10-09 21:32:48 -0700

    added profile page container

--- src/Config/Routes.js
+++ src/Config/Routes.js
@@ -5,6 +5,7 @@ import Home from '../Components/Home';
 import NewRecipeContainer from '../Pages/NewRecipeContainer';
 import RecipeContainer from '../Pages/RecipesContainer';
 import RecipesListContainer from '../Pages/RecipesListContainer';
+import ProfileContainer from '../Pages/ProfileContainer';
 
 export default (
   <Switch>
@@ -12,5 +13,6 @@ export default (
     <Route exact path='/recipes' component={ RecipesListContainer } />
     <Route path='/recipes/new' component={ NewRecipeContainer } />
     <Route exact path='/recipes/:id' component={ RecipeContainer } />
+    <Route exact path='/profile' component={ ProfileContainer } />
   </Switch>
 )
