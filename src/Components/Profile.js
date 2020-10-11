de56e827da3da24a2c0581f7ba4ff4f10fbfd819
TimothyMoy <Timothy.z.moy@gmail.com>
2020-10-10 21:02:43 -0700

    added profile placeholders

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 class Profile extends Component {
   render(){
     return(
-      <div>
-        Profile Page
-      </div>
+      <Container>
+        <Row>
+          <Col xs="2">
+            <img src="https://picsum.photos/100" alt="profile"></img>
+          </Col>
+          <Col xs="10">
+            <h2>Username</h2>
+            <Button className="btn-primary">
+              <a href="/editprofile">
+                Edit Profile
+              </a>
+            </Button>
+          </Col>
+        </Row>
+        <hr/>
+        <Row>
+          <Col xs="4">
+            <h2>About</h2>
+            <p>Let the community know a bit more about you and your recipes!</p>
+          </Col>
+          <Col xs="8">
+            <Row className="rlist">
+              <h2>Your Recipes</h2>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
